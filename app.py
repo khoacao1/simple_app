10e1885e71173cd103e35c81aa6765535dd72c4d
Khoa Cao <khoacao_organ@yahoo.com>
2023-03-15 19:06:50 -0400

    create logic to filter if names  not match input

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+from model.searched_obj import Provider 
 
 app = Flask(__name__)
 
@@ -13,12 +14,14 @@ def results():
     first_name = request.form['first_name']
     city = request.form['city']
     state = request.form['state']
+
+    search_provider = Provider(last_name,first_name,city,state)
     
     API_URL = f'https://npiregistry.cms.hhs.gov/api/?first_name={first_name}&last_name={last_name}&city={city}&state={state}&version=2.1'
     
     counts = 0
     addresses, basics = [],[]
-
+    
     try:
         response = requests.get(API_URL)
         if response.status_code == 200:
@@ -33,7 +36,7 @@ def results():
 
                 basics.append(data['results'][i]['basic'])
 
-            return render_template('results.html', counts=counts, addresses=addresses, basics=basics)
+            return render_template('results.html', counts=counts, addresses=addresses, basics=basics, search_provider=search_provider)
         else:
             print('Error: API returned status code', response.status_code)
     except requests.exceptions.RequestException as e:
---
+++ model/searched_obj.py
@@ -0,0 +1,6 @@
+class Provider:
+    def __init__(self, lastname, firstname, city, state):
+        self.lastname = lastname
+        self.firstname = firstname
+        self.city = city
+        self.state = state
