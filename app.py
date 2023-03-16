1b08b0022a2b57531e9470904be47c52c45c6bc9
Khoa Cao <khoacao_organ@yahoo.com>
2023-03-15 20:01:55 -0400

    modifies: - add logic to clear mailing and location address - get, display othernames that matched input -display addresses

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import requests
-from model.searched_obj import Provider 
 
 app = Flask(__name__)
 
@@ -15,12 +14,12 @@ def results():
     city = request.form['city']
     state = request.form['state']
 
-    search_provider = Provider(last_name,first_name,city,state)
     
     API_URL = f'https://npiregistry.cms.hhs.gov/api/?first_name={first_name}&last_name={last_name}&city={city}&state={state}&version=2.1'
     
     counts = 0
-    addresses, basics = [],[]
+    mailing_address, location_address, basics = [],[],[]
+    othernames = []
     
     try:
         response = requests.get(API_URL)
@@ -29,14 +28,20 @@ def results():
             counts = data['result_count']
                 
             for i in range (0,counts):
-                if "telephone_number" in data['results'][i]['addresses'][0]:
-                    addresses.append(data['results'][i]['addresses'][0])
+                if data['results'][i]['addresses'][0]['address_purpose'] == "MAILING":
+                    mailing_address.append(data['results'][i]['addresses'][0])
+                    location_address.append(data['results'][i]['addresses'][1])
                 else:
-                    addresses.append(data['results'][i]['addresses'][1])
+                    location_address.append(data['results'][i]['addresses'][0])
+                    mailing_address.append(data['results'][i]['addresses'][1])
 
                 basics.append(data['results'][i]['basic'])
+                if len(data['results'][i]['other_names']) > 0:
+                    othernames.append(data['results'][i]['other_names'][0])
+                else:
+                    othernames.append(data['results'][i]['other_names'])
 
-            return render_template('results.html', counts=counts, addresses=addresses, basics=basics, search_provider=search_provider)
+            return render_template('results.html', counts=counts, mailing_address=mailing_address, location_address=location_address, basics=basics, othernames=othernames)
         else:
             print('Error: API returned status code', response.status_code)
     except requests.exceptions.RequestException as e:
--- model/searched_obj.py
+++
@@ -1,6 +0,0 @@
-class Provider:
-    def __init__(self, lastname, firstname, city, state):
-        self.lastname = lastname
-        self.firstname = firstname
-        self.city = city
-        self.state = state
